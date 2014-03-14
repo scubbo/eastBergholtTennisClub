cf9e2507e542453885cc22fad338a5c1b1d61138
Jack Jackson <jackjack@amazon.com>
2014-03-14 21:34:46 +0000

    Dynamic sideBar

--- webdocs/js/main.js
+++ webdocs/js/main.js
@@ -1,2 +1,18 @@
 $(document).ready(function() {
+    $('#sideBar').load('/templates/sideBar.html');
+
+    pageSlug = $(location).attr('pathname').slice(1);
+    if (!(pageSlug)) {
+        pageSlug = 'main';
+    }
+
+
+
+    $('#innerWrapper').load('/templates/content-' + pageSlug + '.html',
+                            function(response, status, xhr) {
+                                if (status == 'error') {
+                                    $('#innerWrapper').load('/templates/content-404.html');
+                                }
+                            });
+
 });
