a17d9f2af758d89036d4bd776cbf32c29e2b70b3
Jack Jackson <jackjack@amazon.com>
2014-03-15 10:44:27 +0000

    Link flyout

--- webdocs/js/main.js
+++ webdocs/js/main.js
@@ -1,18 +1,28 @@
 $(document).ready(function() {
-    $('#sideBar').load('/templates/sideBar.html');
+    $('#sideBar').load('/templates/sideBar.html',
+        function(response,status,xhr) {
+            $('#internalLinks a').each(
+                function(index,item) {
+                    if (item['pathname'] == '/' + pageSlug) {
+                        $(item).addClass('current');
+                        return false; //Break out of loop
+                    }
+                }
+            );
+        }
+      );
 
     pageSlug = $(location).attr('pathname').slice(1);
     if (!(pageSlug)) {
         pageSlug = 'main';
     }
 
-
-
     $('#innerWrapper').load('/templates/content-' + pageSlug + '.html',
-                            function(response, status, xhr) {
-                                if (status == 'error') {
-                                    $('#innerWrapper').load('/templates/content-404.html');
-                                }
-                            });
+        function(response, status, xhr) {
+            if (status == 'error') {
+                $('#innerWrapper').load('/templates/content-404.html');
+            }
+        }
+    );
 
 });
